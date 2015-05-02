5433fbdf0281adfb0b53a520f632e5c4e0cce297
Nam Chu Hoai <nambrot@googlemail.com>
2015-05-02 16:59:58 -0700

    REmove postgiscreate statement

--- db/migrate/20150502223248_use_post_gis_column.rb
+++ db/migrate/20150502223248_use_post_gis_column.rb
@@ -1,6 +1,5 @@
 class UsePostGisColumn < ActiveRecord::Migration
   def change
-    execute "CREATE EXTENSION postgis;"
     remove_column :locations, :lat
     remove_column :locations, :lng
     add_column :locations, :lonlat, :st_point, geographic: true
